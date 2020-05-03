b61d2ef45975eb83b6706f99f5f7fa498a9f866e
Praneeth Babu Marella <9484118+pmarella2@users.noreply.github.com>
2020-05-02 21:35:47 -0400

    Update handler.py

--- channels/handler.py
+++ channels/handler.py
@@ -12,7 +12,7 @@ from django import http
 from django.conf import settings
 from django.core import signals
 from django.core.handlers import base
-from django.core.urlresolvers import set_script_prefix
+from django.urls import *
 from django.http import FileResponse, HttpResponse, HttpResponseServerError
 from django.utils import six
 from django.utils.functional import cached_property
